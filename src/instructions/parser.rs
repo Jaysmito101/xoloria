cef34ba66855090fbe846e6cb6339642e96ec701
Jaysmito101 <jaysmito101@gmail.com>
2026-06-12 01:11:50 +0530

    feat: implement 64-bit register operations in instruction parser and add test cases for shift instructions

--- src/instructions/parser.rs
+++ src/instructions/parser.rs
@@ -301,6 +301,37 @@ impl Instruction {
             _ => Err(InstructionError::InvalidInstruction),
         }
     }
+
+    fn try_op_reg64(raw: payload::RType) -> InstructionResult<Self> {
+        match (raw.funct3, raw.funct7) {
+            (0, 0) => Ok(Self::Addw {
+                rd: raw.rd,
+                rs1: raw.rs1,
+                rs2: raw.rs2,
+            }),
+            (0, 0x20) => Ok(Self::Subw {
+                rd: raw.rd,
+                rs1: raw.rs1,
+                rs2: raw.rs2,
+            }),
+            (1, 0) => Ok(Self::Sllw {
+                rd: raw.rd,
+                rs1: raw.rs1,
+                rs2: raw.rs2,
+            }),
+            (5, 0) => Ok(Self::Srlw {
+                rd: raw.rd,
+                rs1: raw.rs1,
+                rs2: raw.rs2,
+            }),
+            (5, 0x20) => Ok(Self::Sraw {
+                rd: raw.rd,
+                rs1: raw.rs1,
+                rs2: raw.rs2,
+            }),
+            _ => Err(InstructionError::InvalidInstruction),
+        }
+    }
 }
 
 impl TryFrom<u32> for Instruction {
@@ -318,10 +349,7 @@ impl TryFrom<u32> for Instruction {
             OpcodeName::Jalr => Self::try_jalr(payload::IType::try_from(value)?),
             OpcodeName::Jal => Self::try_jal(payload::JType::try_from(value)?),
             OpcodeName::OpImm64 => Self::try_op_imm64(payload::IType::try_from(value)?),
-            OpcodeName::OpReg64 => {
-                tracing::info!("Parsing OpReg64 instruction with value {:#010x}", value);
-                todo!()
-            }
+            OpcodeName::OpReg64 => Self::try_op_reg64(payload::RType::try_from(value)?),
             OpcodeName::Atomic => {
                 tracing::info!("Parsing Atomic instruction with value {:#010x}", value);
                 todo!()
