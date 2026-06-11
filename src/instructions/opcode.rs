d8b1da76c23876c8a75d83e739fc3dff1c1c7f90
Jaysmito101 <jaysmito101@gmail.com>
2026-06-11 23:16:12 +0530

    feat: implement TryFrom<u32> for OpcodeName to handle opcode conversion

--- src/instructions/opcode.rs
+++ src/instructions/opcode.rs
@@ -1,3 +1,5 @@
+use crate::instructions::{InstructionError, InstructionResult};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum OpcodeName {
     // RV32I Base
@@ -21,4 +23,29 @@ pub enum OpcodeName {
     // System, Zicsr, Zifencei Extensions
     Fence = 15,
     System = 115,
+}
+
+impl TryFrom<u32> for OpcodeName {
+    type Error = InstructionError;
+
+    #[inline(always)]
+    fn try_from(value: u32) -> InstructionResult<Self> {
+        match value & 0x7f {
+            3 => Ok(Self::Load),
+            19 => Ok(Self::OpImm),
+            23 => Ok(Self::Auipc),
+            35 => Ok(Self::Store),
+            51 => Ok(Self::OpReg),
+            55 => Ok(Self::Lui),
+            99 => Ok(Self::Branch),
+            103 => Ok(Self::Jalr),
+            111 => Ok(Self::Jal),
+            27 => Ok(Self::OpImm64),
+            59 => Ok(Self::OpReg64),
+            47 => Ok(Self::Atomic),
+            15 => Ok(Self::Fence),
+            115 => Ok(Self::System),
+            other => Err(InstructionError::UnknownOpcode(other as u8)),
+        }
+    }
 }
