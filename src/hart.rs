df9d020361c298ffe118eaeef1551c8e2d5a7f50
Jaysmito101 <jaysmito101@gmail.com>
2026-06-13 23:54:01 +0530

    feat: enhance display implementation for Hart and HartRegisters

--- src/hart.rs
+++ src/hart.rs
@@ -1,7 +1,9 @@
+use std::fmt::Display;
+
 use crate::{
     Bus, BusIO, Result,
     instructions::Instruction,
-    registers::{ISAExtensions, Misa, Register},
+    registers::{GeneralRegisterName, ISAExtensions, Misa, Register},
     vm::{self, VmOutput},
 };
 
@@ -10,13 +12,6 @@ pub enum PrivilageMode {
     Machine = 0,
     Supervisor = 1,
     User = 2,
-}
-
-pub struct RegisterSet {
-    pub(crate) pc: Register,
-    pub(crate) x: [Register; 32],
-    // the csr
-    pub(crate) csr: [Register; 4096],
 }
 
 #[derive(Debug)]
@@ -50,10 +45,62 @@ pub struct HartRegisters {
     pub(crate) satp: Register,     // supervisor address translation and protection
 }
 
+impl Display for HartRegisters {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Program Counter: {:#x}", self.pc)?;
+        write!(f, "General Registers: {{ ")?;
+        for (i, &value) in self.x.iter().enumerate() {
+            write!(
+                f,
+                "{}: {:#x} ({}){}",
+                GeneralRegisterName::try_from(i as u8).unwrap_or(GeneralRegisterName::Zero),
+                // i,
+                value,
+                value,
+                if i != self.x.len() - 1 { ", " } else { "" }
+            )?;
+        }
+        writeln!(f, " }}")?;
+        write!(
+            f,
+            "System Registers: {{ mhartid: {:#x} ({}), ",
+            self.mhartid, self.mhartid
+        )?;
+        write!(f, "misa: {:#x} ({}), ", self.misa, self.misa)?;
+        write!(f, "mstatus: {:#x} ({}), ", self.mstatus, self.mstatus)?;
+        write!(f, "medeleg: {:#x} ({}), ", self.medeleg, self.medeleg)?;
+        write!(f, "mideleg: {:#x} ({}), ", self.mideleg, self.mideleg)?;
+        write!(f, "mie: {:#x} ({}), ", self.mie, self.mie)?;
+        write!(f, "mtvec: {:#x} ({}), ", self.mtvec, self.mtvec)?;
+        write!(f, "mscratch: {:#x} ({}), ", self.mscratch, self.mscratch)?;
+        write!(f, "mepc: {:#x} ({}), ", self.mepc, self.mepc)?;
+        write!(f, "mcause: {:#x} ({}), ", self.mcause, self.mcause)?;
+        write!(f, "mtval: {:#x} ({}), ", self.mtval, self.mtval)?;
+        write!(f, "mip: {:#x} ({}), ", self.mip, self.mip)?;
+        write!(f, "stvec: {:#x} ({}), ", self.stvec, self.stvec)?;
+        write!(f, "sscratch: {:#x} ({}), ", self.sscratch, self.sscratch)?;
+        write!(f, "sepc: {:#x} ({}), ", self.sepc, self.sepc)?;
+        write!(f, "scause: {:#x} ({}), ", self.scause, self.scause)?;
+        write!(f, "stval: {:#x} ({}), ", self.stval, self.stval)?;
+        write!(f, "satp: {:#x} ({}) }}", self.satp, self.satp)?;
+        write!(f, "}}")?;
+
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 pub struct Hart {
     pub(crate) privilage_mode: PrivilageMode,
     pub(crate) registers: HartRegisters,
+}
+
+impl Display for Hart {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Privilage Mode: {:?}", self.privilage_mode)?;
+        writeln!(f, "Registers:\n{}", self.registers)?;
+        Ok(())
+    }
 }
 
 impl Hart {
@@ -114,7 +161,7 @@ impl Hart {
 
             // a way to debug register state with this for now
             Instruction::Ecall => {
-                tracing::info!("{:?}", &self);
+                tracing::info!("{}", &self);
                 Ok(VmOutput::NextInstruction)
             }
             _ => unimplemented!("Instruction {:?} not implemented", instruction),
