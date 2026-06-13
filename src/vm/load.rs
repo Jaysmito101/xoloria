992243fe4f329179d500811608d0159adde41eb1
Jaysmito101 <jaysmito101@gmail.com>
2026-06-14 00:05:51 +0530

    feat: add inline annotations for LUI and AUIPC instruction execution

--- src/vm/load.rs
+++ src/vm/load.rs
@@ -4,11 +4,13 @@ use crate::{
     vm::{VmOutput, VmResult},
 };
 
+#[inline(always)]
 pub fn execute_lui(rd: GeneralRegisterName, imm: i32, hart: &mut Hart) -> VmResult {
     hart.registers.x[rd as usize] = imm as u64;
     Ok(VmOutput::NextInstruction)
 }
 
+#[inline(always)]
 pub fn execute_auipc(rd: GeneralRegisterName, imm: i32, hart: &mut Hart) -> VmResult {
     hart.registers.x[rd as usize] = (imm as i64 + hart.registers.pc as i64) as Register;
     Ok(VmOutput::NextInstruction)
