ffa348da66b31a3f371d1bdad311e67dcaab8f08
Jaysmito101 <jaysmito101@gmail.com>
2026-06-28 00:33:00 +0530

    feat: simpligy register name impls

--- crates/emulator/src/registers/names.rs
+++ crates/emulator/src/registers/names.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use strum::EnumIter;
+use strum::{EnumIter, IntoEnumIterator};
 
 #[derive(EnumIter, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum GeneralRegisterName {
@@ -78,41 +78,12 @@ impl TryFrom<u8> for GeneralRegisterName {
     type Error = ();
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(Self::Zero),
-            1 => Ok(Self::Ra),
-            2 => Ok(Self::Sp),
-            3 => Ok(Self::Gp),
-            4 => Ok(Self::Tp),
-            5 => Ok(Self::T0),
-            6 => Ok(Self::T1),
-            7 => Ok(Self::T2),
-            8 => Ok(Self::S0),
-            9 => Ok(Self::S1),
-            10 => Ok(Self::A0),
-            11 => Ok(Self::A1),
-            12 => Ok(Self::A2),
-            13 => Ok(Self::A3),
-            14 => Ok(Self::A4),
-            15 => Ok(Self::A5),
-            16 => Ok(Self::A6),
-            17 => Ok(Self::A7),
-            18 => Ok(Self::S2),
-            19 => Ok(Self::S3),
-            20 => Ok(Self::S4),
-            21 => Ok(Self::S5),
-            22 => Ok(Self::S6),
-            23 => Ok(Self::S7),
-            24 => Ok(Self::S8),
-            25 => Ok(Self::S9),
-            26 => Ok(Self::S10),
-            27 => Ok(Self::S11),
-            28 => Ok(Self::T3),
-            29 => Ok(Self::T4),
-            30 => Ok(Self::T5),
-            31 => Ok(Self::T6),
-            _ => Err(()),
+        for name in GeneralRegisterName::iter() {
+            if name as u8 == value {
+                return Ok(name);
+            }
         }
+        Err(())
     }
 }
 
@@ -123,26 +94,11 @@ impl TryFrom<u16> for ControlRegisterName {
         if value > 4096 {
             return Err(());
         }
-        match value {
-            0xF14 => Ok(Self::Mhartid),
-            0x300 => Ok(Self::Mstatus),
-            0x301 => Ok(Self::Misa),
-            0x302 => Ok(Self::Medeleg),
-            0x303 => Ok(Self::Mideleg),
-            0x304 => Ok(Self::Mie),
-            0x305 => Ok(Self::Mtvec),
-            0x340 => Ok(Self::Mscratch),
-            0x341 => Ok(Self::Mepc),
-            0x342 => Ok(Self::Mcause),
-            0x343 => Ok(Self::Mtval),
-            0x344 => Ok(Self::Mip),
-            0x105 => Ok(Self::Stvec),
-            0x140 => Ok(Self::Sscratch),
-            0x141 => Ok(Self::Sepc),
-            0x142 => Ok(Self::Scause),
-            0x143 => Ok(Self::Stval),
-            0x180 => Ok(Self::Satp),
-            _ => Err(()),
+        for name in ControlRegisterName::iter() {
+            if name as u16 == value {
+                return Ok(name);
+            }
         }
+        Err(())
     }
 }
