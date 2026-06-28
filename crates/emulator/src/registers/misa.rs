2baecf6043febf137aa126b2262b2e35ea89e766
Jaysmito101 <jaysmito101@gmail.com>
2026-06-29 00:37:28 +0530

    feat: use bitset for with extenstion

--- crates/emulator/src/registers/misa.rs
+++ crates/emulator/src/registers/misa.rs
@@ -1,27 +1,29 @@
 use macros::RegisterBits;
 
+use crate::registers::RegisterBits;
+
 use super::Register;
 
 #[derive(RegisterBits, Default)]
 pub struct Misa(Register);
 
 pub enum ISAExtensions {
-    A = 1 << 0,  // Atomic extension
-    B = 1 << 1,  // B extension
-    C = 1 << 2,  // Compressed extension
-    D = 1 << 3,  // Double-precision floating-point extension
-    E = 1 << 4,  // RV32E/64E base ISA
-    F = 1 << 5,  // Single-precision floating-point extension
-    H = 1 << 7,  // Hypervisor extension
-    I = 1 << 8,  // RV32I/64I base ISA
-    M = 1 << 12, // Integer Multiply/Divide extension
-    N = 1 << 13, // Tentatively reserved for User-Level Interrupts extension
-    P = 1 << 15, // Tentatively reserved for Packed-SIMD extension
-    Q = 1 << 16, // Quad-precision floating-point extension
-    S = 1 << 18, // Supervisor mode implemented
-    U = 1 << 20, // User mode implemented
-    V = 1 << 21, // Vector extension
-    X = 1 << 23, // Non-standard extensions present
+    A = 0,  // Atomic extension
+    B = 1,  // B extension
+    C = 2,  // Compressed extension
+    D = 3,  // Double-precision floating-point extension
+    E = 4,  // RV32E/64E base ISA
+    F = 5,  // Single-precision floating-point extension
+    H = 7,  // Hypervisor extension
+    I = 8,  // RV32I/64I base ISA
+    M = 12, // Integer Multiply/Divide extension
+    N = 13, // Tentatively reserved for User-Level Interrupts extension
+    P = 15, // Tentatively reserved for Packed-SIMD extension
+    Q = 16, // Quad-precision floating-point extension
+    S = 18, // Supervisor mode implemented
+    U = 20, // User mode implemented
+    V = 21, // Vector extension
+    X = 23, // Non-standard extensions present
 }
 
 impl Misa {
@@ -37,7 +39,7 @@ impl Misa {
     }
 
     pub fn with_extension(mut self, ext: ISAExtensions) -> Self {
-        self.0 |= ext as Register;
+        self.bitset(ext as u8);
         self
     }
 }
