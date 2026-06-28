9823049c6dff85f5e5203073f49c1553f185ed4c
Jaysmito101 <jaysmito101@gmail.com>
2026-06-28 21:30:38 +0530

    feat: simplfy csr register read/write

--- crates/emulator/src/registers/csr.rs
+++ crates/emulator/src/registers/csr.rs
@@ -1,25 +1,21 @@
-use std::sync::atomic::Ordering;
-
 use crate::{
     PrivilageMode,
     instructions::InstructionResult,
-    registers::{AtomicRegister, ControlRegisterName, Register},
+    registers::{ControlRegisterName, Register},
 };
 
 #[derive(Debug)]
 pub struct ControlStatusRegisters {
-    regs: [AtomicRegister; 4096],
+    regs: [Register; 4096],
 }
 
 impl ControlStatusRegisters {
     pub fn new() -> Self {
-        Self {
-            regs: [const { AtomicRegister::new(0) }; 4096],
-        }
+        Self { regs: [0; 4096] }
     }
 
-    pub fn with(self, name: ControlRegisterName, value: Register) -> Self {
-        self.regs[name as usize].store(value, std::sync::atomic::Ordering::SeqCst);
+    pub fn with(mut self, name: ControlRegisterName, value: Register) -> Self {
+        self.regs[name as usize] = value;
         self
     }
 
@@ -28,40 +24,22 @@ impl ControlStatusRegisters {
         name: ControlRegisterName,
         privilage: PrivilageMode,
     ) -> InstructionResult<Register> {
-        self.rmw(name, privilage, |value| value)
+        Ok(self.regs[name as usize])
     }
 
     pub fn write(
-        &self,
+        &mut self,
         name: ControlRegisterName,
         value: Register,
         privilage: PrivilageMode,
     ) -> InstructionResult<()> {
-        self.rmw(name, privilage, |_| value).map(|_| ())
+        self.regs[name as usize] = value;
+        Ok(())
     }
+}
 
-    fn rmw<Op>(
-        &self,
-        name: ControlRegisterName,
-        _privilage: PrivilageMode,
-        op: Op,
-    ) -> InstructionResult<Register>
-    where
-        Op: Fn(Register) -> Register,
-    {
-        let mut old_value = self.regs[name as usize].load(Ordering::Acquire);
-        loop {
-            let new_value = op(old_value);
-            match self.regs[name as usize].compare_exchange(
-                old_value,
-                new_value,
-                Ordering::AcqRel,
-                Ordering::Acquire,
-            ) {
-                Ok(_) => break,
-                Err(current) => old_value = current,
-            }
-        }
-        Ok(old_value)
+impl Default for ControlStatusRegisters {
+    fn default() -> Self {
+        Self::new()
     }
 }
