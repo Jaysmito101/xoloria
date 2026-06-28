e6c435bc00d1380748e553b4bcce1d5c94822863
Jaysmito101 <jaysmito101@gmail.com>
2026-06-29 01:40:00 +0530

    feat: allow mip in csr rw

--- crates/emulator/src/registers/csr.rs
+++ crates/emulator/src/registers/csr.rs
@@ -30,7 +30,7 @@ impl ControlStatusRegisters {
         }
 
         match name {
-            Mie => Ok(self.regs[name as usize]),
+            Mie | Mip => Ok(self.regs[name as usize]),
             _ => Err(RegisterError::InvalidCSRRead(name, privilage)),
         }
     }
@@ -52,7 +52,7 @@ impl ControlStatusRegisters {
         }
 
         match name {
-            Mie => {
+            Mie | Mip => {
                 self.regs[name as usize] = value;
                 Ok(())
             }
