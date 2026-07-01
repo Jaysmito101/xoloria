866a447bcd3b65fa206643c6595bbd98689c0ddc
Jaysmito101 <jaysmito101@gmail.com>
2026-07-01 11:18:56 +0530

    feat: refactor Mtvec register implementation to use enums and macros

--- crates/emulator/src/registers/mtvec.rs
+++ crates/emulator/src/registers/mtvec.rs
@@ -1,31 +1,35 @@
-use macros::RegisterBits;
+use macros::register;
 
-use crate::{Address, registers::Register};
+use crate::Address;
 
-#[derive(RegisterBits)]
-pub struct Mtvec(Register);
-
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum MtvecMode {
     Direct = 0,
     Vectored = 1,
     Reserved = 2,
 }
 
-impl Mtvec {
-    pub const MODE: u8 = 0;
-    pub const BASE: u8 = 2;
-
+impl crate::registers::FromBits for MtvecMode {
     #[inline(always)]
-    pub fn mode(&self) -> MtvecMode {
-        match self.0 & 0b11 {
+    fn from_bits(bits: u64) -> Self {
+        match bits {
             0 => MtvecMode::Direct,
             1 => MtvecMode::Vectored,
             _ => MtvecMode::Reserved,
         }
     }
+}
 
+impl crate::registers::IntoBits for MtvecMode {
     #[inline(always)]
-    pub fn base(&self) -> Address {
-        self.0 & !0b11
+    fn into_bits(self) -> u64 {
+        self as u64
+    }
+}
+
+register! {
+    pub register Mtvec {
+        pub mode: MtvecMode = range(0..=1),
+        pub base: Address = range(2..=63),
     }
 }
