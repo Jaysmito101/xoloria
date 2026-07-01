c35772cf2baa7d2a2e592a6f67b267c7605c8b1b
Jaysmito101 <jaysmito101@gmail.com>
2026-07-01 11:14:06 +0530

    feat: refactor Mie register implementation using macros

--- crates/emulator/src/registers/mie.rs
+++ crates/emulator/src/registers/mie.rs
@@ -1,51 +1,13 @@
-use macros::RegisterBits;
+use macros::register;
 
-use crate::registers::Register;
-
-#[derive(RegisterBits)]
-pub struct Mie(Register);
-
-impl Mie {
-    pub const SSIE: u8 = 1;
-    pub const MSIE: u8 = 3;
-    pub const STIE: u8 = 5;
-    pub const MTIE: u8 = 7;
-    pub const SEIE: u8 = 9;
-    pub const MEIE: u8 = 11;
-    pub const LCOFIE: u8 = 13;
-
-    #[inline(always)]
-    pub fn ssie(&self) -> bool {
-        self.bit(Self::SSIE)
-    }
-
-    #[inline(always)]
-    pub fn msie(&self) -> bool {
-        self.bit(Self::MSIE)
-    }
-
-    #[inline(always)]
-    pub fn stie(&self) -> bool {
-        self.bit(Self::STIE)
-    }
-
-    #[inline(always)]
-    pub fn mtie(&self) -> bool {
-        self.bit(Self::MTIE)
-    }
-
-    #[inline(always)]
-    pub fn seie(&self) -> bool {
-        self.bit(Self::SEIE)
-    }
-
-    #[inline(always)]
-    pub fn meie(&self) -> bool {
-        self.bit(Self::MEIE)
-    }
-
-    #[inline(always)]
-    pub fn lcofie(&self) -> bool {
-        self.bit(Self::LCOFIE)
+register! {
+    pub register Mie {
+        pub ssie: bool = bit(1),
+        pub msie: bool = bit(3),
+        pub stie: bool = bit(5),
+        pub mtie: bool = bit(7),
+        pub seie: bool = bit(9),
+        pub meie: bool = bit(11),
+        pub lcofie: bool = bit(13),
     }
 }
